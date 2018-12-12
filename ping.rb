faaa747cc8fde2da520250c84c7d6d1a00288f14
Paul Schaefer <yeppaul2@gmail.com>
2018-12-12 17:45:24 -0500

    Added timed response example and PM example

--- ping.rb
+++ ping.rb
@@ -10,7 +10,22 @@ bot = Discordrb::Bot.new token: JSON.parse(File.read('.config'))['token']
 # that exactly contains the string "Ping!".
 # The code inside it will be executed, and a "Pong!" response will be sent.
 bot.message(content: 'Ping!') do |event|
-  event.respond 'Pong!'
+  # The `respond` method returns a `Message` object, which is stored in a
+  # variable `m`. The `edit` method is then called to edit the message with the
+  # time difference between when the event was received and after the message
+  # was sent.
+  m = event.respond('Pong!')
+  m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
+end
+
+# The `mention` event is called if the bot is *directly mentioned*, i.e. not
+# using a role mention or @everyone/@here.
+bot.mention do |event|
+  # The `pm` method is used to send a private message (also called a DM or
+  # direct message) to the user who sent the initial message.
+  event.user.pm('You have mentioned me!')
+
+  event.respond("You have mentioned me: @#{event.user.username}##{event.user.discriminator}")
 end
 
 # This method call has to be put at the end of your script,
