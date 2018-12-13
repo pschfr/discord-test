11179e003629cbdbc6b927f95392c96fda104e49
Paul Schaefer <yeppaul2@gmail.com>
2018-12-12 20:27:37 -0500

    simplified event.respond string, and satisfied rubocop

--- ping.rb
+++ ping.rb
@@ -11,15 +11,12 @@ bot = Discordrb::Bot.new token: JSON.parse(File.read('.config'))['token']
 
 # Upon recieving --help, do this...
 bot.message(content: '--help') do |event|
-  # Construct help message string
-  help_message = %(
+  # Send message, and store it that we can issue a delete request later
+  message = event.respond %(
     Hello! This is a test bot.\n
     The time is: #{Time.now.strftime('%F %T %Z')}.\n
     If you click the #{CROSS_MARK}, this message will disappear.
   )
-
-  # Send message, and store it that we can issue a delete request later
-  message = event.respond help_message
 
   # React to the message to give a user an easy "button" to press
   message.react CROSS_MARK
@@ -27,7 +24,11 @@ bot.message(content: '--help') do |event|
   # Add an await for a ReactionAddEvent, that will only trigger for reactions
   # that match our CROSS_MARK emoji. This time, I'm using interpolation to make
   # the await key unique for this event so that multiple awaits can exist.
-  bot.add_await(:"delete_#{message.id}", Discordrb::Events::ReactionAddEvent, emoji: CROSS_MARK) do |reaction_event|
+  bot.add_await(
+    :"delete_#{message.id}",
+    Discordrb::Events::ReactionAddEvent,
+    emoji: CROSS_MARK
+  ) do |reaction_event|
     # Since this code will run on every CROSS_MARK reaction, it might not
     # be on our time message we sent earlier. We use `next` to skip the rest
     # of the block unless it was our message that was reacted to.
